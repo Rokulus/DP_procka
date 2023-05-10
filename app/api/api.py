cd2b3f2189e2b3d2e9fa4ebaa2582d2756ea16fd
Rokulus <76742480+Rokulus@users.noreply.github.com>
2023-05-10 14:23:57 +0200

    Fix router

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.endpoints import auth, users, fmuJS, fmu, matlab
+from app.api.endpoints import auth, users, fmuJS, fmu, matlab, logs
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["Authorization"])
@@ -8,4 +8,4 @@ api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(fmuJS.router, prefix="/fmuJS", tags =["FMU Javascript"])
 api_router.include_router(fmu.router, prefix="/fmu", tags=["FMU"])
 api_router.include_router(matlab.router, prefix="/matlab", tags=["Matlab"])
-api_router.include_router(matlab.router, prefix="/logs", tags=["Logs"])
+api_router.include_router(logs.router, prefix="/logs", tags=["Logs"])
